1c70b3cc4eae3d8c9ea13fb59e9fcbdcd7ca9ce6
Ahmad shah <shadan3000@gmail.com>
2022-09-27 09:54:15 -0500

    updated

--- script.js
+++ script.js
@@ -3,14 +3,37 @@
 // 1
 console.log(8); //8
 // 2
-//  Data type (PRIMITIVES, Strings, nulln, Booleans and Undefined).
+ /* Data type (
+    Number, 
+    Strings, 
+    nulln, 
+    Booleans 
+   Undefined).
+
+*/
 // 3
-//  Operators Sambols ("+", "-", "*", "/", "%", "**")
+/* Operators Sambols ("+", "-", "*", "/", "%", "**")
+ Additionn is +
+ substraction is _
+ multiplication is *
+ dividing is /
+ Modulus is %
+ Dxponentation is **
+
+ Extra
+
+ incremnent ++
+ Decrement --
+*/
+
 // 4
 console.log(1%5); //1
 console.log(10%3); //1
+console.log(5%4); //1
+console.log(7%2); //1
+
 // 5
-console.log(' I AM MARY POPPINS, YALL'); //  I AM MARY POPPINS, YALL
+console.log(' I\'M MARY POPPINS, Y\'ALL'); //  I'M MARY POPPINS, Y'ALL
 // 6
 console.log("Hawk " + "eye"); // Hawk eye
 // 7
@@ -20,10 +43,41 @@ console.log("Avengers;Endgame"[15]); //e
 
 // Bonus
 // 1 
-// // NOTE: The backslash will NOT appear in the terminal/console
+console.log("\/"); //  "/"
+// NOTE: The backslash will NOT appear in the terminal/console
 // 2
 console.log(5**4-"9"); //616
+// 5*5*5*5=
+//  625-9= 616
+625
 // 3
 console.log("I".length - "Am".length + "Iron".length - "Man".length); //0
+//  1 - 2 + 4 - 3 = 0
+// // 1+2+4+3=10
+console.log(("I".length + "Am".length + "Iron".length + "Man".length) %10); //0
+// 1+2+4+3=10
+console.log(("I".length + "Am".length + "Iron".length + "Man".length) * 0); //0
+
+//  10*0 = 0
 // 4
-console.log("AVENGERS");
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5 
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
+// With LET
+
+// let m = 25;
+// let c = 4;
+// let u = m + c;
+// console.log(u);
+//  In this two concept we have the same variable  (m = 25) because of this the result in the terminal will be error 
