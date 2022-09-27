5f22119a0babc0951bc3f31febed9b6a83b85139
Ahmad shah <shadan3000@gmail.com>
2022-09-27 00:36:02 -0500

    JavaScriptBasicExercise

---
+++ script.js
@@ -0,0 +1,29 @@
+// (NOTE: Console logs are not necessary unless specifically asked for. However, they do help to make sure answers are correct.)
+
+// 1
+console.log(8); //8
+// 2
+//  Data type (PRIMITIVES, Strings, nulln, Booleans and Undefined).
+// 3
+//  Operators Sambols ("+", "-", "*", "/", "%", "**")
+// 4
+console.log(1%5); //1
+console.log(10%3); //1
+// 5
+console.log(' I AM MARY POPPINS, YALL'); //  I AM MARY POPPINS, YALL
+// 6
+console.log("Hawk " + "eye"); // Hawk eye
+// 7
+console.log("Ahmad Shah".length + "Shadan".length); // 16
+// 8
+console.log("Avengers;Endgame"[15]); //e
+
+// Bonus
+// 1 
+// // NOTE: The backslash will NOT appear in the terminal/console
+// 2
+console.log(5**4-"9"); //616
+// 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length); //0
+// 4
+console.log("AVENGERS");
